5bbe545168a1196fd467376fa46a09c65f7fded4
cryptopandamc <junetefl@gmail.com>
2022-04-06 13:43:56 +0100

    adding a question no longer adds unwanted tags to the tag table

--- src/components/FormikClass.jsx
+++ src/components/FormikClass.jsx
@@ -1,154 +1,168 @@
 import React, { Component } from 'react';
-import TagService from '../service/TagService';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import RadioButton from './RadioButton';
 import QuestionService from '../service/QuestionService'
+import TagService from '../service/TagService'
 
 
-    class FormikClass extends Component {
+class FormikClass extends Component {
 
-        constructor(props) {
-            super(props)
-            this.state = {
-                questionText: '',
-                answers: [],
-                correctAnswer: '',
-                tags: []
-            }
-            this.retrieveAllTags = this.retrieveAllTags.bind(this)
-            this.addQuestion = this.addQuestion.bind(this)
+    constructor(props) {
+        super(props)
+        this.state = {
+            questionText: '',
+            answers: [],
+            correctAnswer: '',
+            tags: []
         }
+        this.retrieveAllTags = this.retrieveAllTags.bind(this)
+        this.addQuestion = this.addQuestion.bind(this)
+    }
 
-        componentDidMount() {
-            this.retrieveAllTags();
-        }
+    componentDidMount() {
+        this.retrieveAllTags();
+    }
 
-        retrieveAllTags = () => {
-            TagService.retrieveAllTags()
-                .then(
-                    response => {
-                        this.setState({
-                            tags: response.data
-                        })
-                    }
-                )
-        }
+    retrieveAllTags = () => {
+        TagService.retrieveAllTags()
+            .then(
+                response => {
+                    this.setState({
+                        tags: response.data
+                    })
+                }
+            )
+    }
 
-        addQuestion = (question)  => {
-            QuestionService.AddQuestion(question)
+    addQuestion = (question) => {
+        QuestionService.AddQuestion(question)
             .then(() => this.props.history.push('/QuestionList'))
-        } 
+    }
 
-        render() {
-            return (
+    getTagsById = (tags) => {
+        let newTags = [];
+        tags.map((tagId) => {
+            newTags.push(this.state.tags.filter((tag) => {
+                return (tag.tagId === Number(tagId)) //tag.tagId > Number(tagId) 
+            })[0]
+            )
+            return "hello";
+        })
+        return newTags;
+    }
 
-                <Formik
+    render() {
+        return (
 
-                    initialValues={{ questionText: '', tags: [], answers: [], correctAnswer: '' }}
-                    validationSchema={Yup.object({
-                        questionText: Yup.string()
-                            .required('Required'),
-                        tags: Yup.array().of(Yup.string())
-                            .required('Required'),
-                        answers: Yup.array().of(Yup.string())
-                            .required('Required'),
-                        correctAnswer: Yup.string()
-                            .required('Required')
-                    })}
+            <Formik
 
-                    onSubmit={(values, { setSubmitting }) => {
-                        this.addQuestion(values);
-                        console.log(values)
-                        setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            setSubmitting(false);
-                        }, 400);
+                initialValues={{ questionText: '', tags: [], answers: [], correctAnswer: '' }}
+                validationSchema={Yup.object({
+                    questionText: Yup.string()
+                        .required('Required'),
+                    tags: Yup.array().of(Yup.string())
+                        .required('Required'),
+                    answers: Yup.array().of(Yup.string())
+                        .required('Required'),
+                    correctAnswer: Yup.string()
+                        .required('Required')
+                })}
 
-                    }}
+                onSubmit={(values, { setSubmitting }) => {
+                    let packet = { ...values };
+                    let tags = this.getTagsById(values.tags);
+                    packet.tags = tags;
+                    this.addQuestion(packet);
+                    setTimeout(() => {
+                        alert(JSON.stringify(packet, null, 2));
+                        setSubmitting(false);
+                    }, 400);
 
-                >
-                    <div>
-                        <Form>
-                            <label htmlFor="questionText">Question Text</label>
-                            <Field className="form-control" type="text" name="questionText" required></Field>
-                            <ErrorMessage name="questionText" />
+                }}
 
-                            <label htmlFor="tags">Add topics</label>
-                            <Field as="select" name="tags" multiple>
-                                {this.state.tags.map((tag) => (
-                                    <option key={tag.tagId} value={tag.tagId, tag.name}>
-                                        {tag.name}
-                                    </option>
-                                ))}
-                            </Field >
-                            <ErrorMessage name="tags" />
+            >
+                <div>
+                    <Form>
+                        <label htmlFor="questionText">Question Text</label>
+                        <Field className="form-control" type="text" name="questionText" required></Field>
+                        <ErrorMessage name="questionText" />
 
+                        <label htmlFor="tags">Add topics</label>
+                        <Field as="select" name="tags" multiple>
+                            {this.state.tags.map((tag) => (
+                                <option key={tag.tagId} value={tag.tagId}>
+                                    {tag.name}
+                                </option>
+                            ))}
+                        </Field >
+                        <ErrorMessage name="tags" />
+
+                        <div>
+                            <label>Answer A</label>
                             <div>
-                                <label>Answer A</label>
-                                <div>
-                                    <Field name="answers[0]" required />
-                                    <ErrorMessage name="answers" />
-                                </div>
-                                <div>
-                                    <label>Answer B</label>
-                                </div>
-                                <div>
-                                    <Field name="answers[1]" required />
-                                    <ErrorMessage name="answers" />
-                                </div>
-                                <div>
-                                    <label>Answer C</label>
-                                </div>
-                                <div>
-                                    <Field name="answers[2]" required />
-                                    <ErrorMessage name="answers" />
-                                </div>
-                                <div>
-                                    <label>Answer D</label>
-                                </div>
-                                <div>
-                                    <Field name="answers[3]" required />
-                                    <ErrorMessage name="answers" />
-                                </div>
+                                <Field name="answers[0]" required />
+                                <ErrorMessage name="answers" />
                             </div>
+                            <div>
+                                <label>Answer B</label>
+                            </div>
+                            <div>
+                                <Field name="answers[1]" required />
+                                <ErrorMessage name="answers" />
+                            </div>
+                            <div>
+                                <label>Answer C</label>
+                            </div>
+                            <div>
+                                <Field name="answers[2]" required />
+                                <ErrorMessage name="answers" />
+                            </div>
+                            <div>
+                                <label>Answer D</label>
+                            </div>
+                            <div>
+                                <Field name="answers[3]" required />
+                                <ErrorMessage name="answers" />
+                            </div>
+                        </div>
 
 
-                            <Field
-                                component={RadioButton}
-                                name="correctAnswer"
-                                id="A"
-                                label="A"
-                            />
-                            <Field
-                                component={RadioButton}
-                                name="correctAnswer"
-                                id="B"
-                                label="B"
-                            />
-                            <Field
-                                component={RadioButton}
-                                name="correctAnswer"
-                                id="C"
-                                label="C"
-                            />
-                            <Field
-                                component={RadioButton}
-                                name="correctAnswer"
-                                id="D"
-                                label="D"
-                            />
-                            <button type="submit">Submit</button>
+                        <Field
+                            component={RadioButton}
+                            name="correctAnswer"
+                            id="A"
+                            label="A"
+                        />
+                        <Field
+                            component={RadioButton}
+                            name="correctAnswer"
+                            id="B"
+                            label="B"
+                        />
+                        <Field
+                            component={RadioButton}
+                            name="correctAnswer"
+                            id="C"
+                            label="C"
+                        />
+                        <Field
+                            component={RadioButton}
+                            name="correctAnswer"
+                            id="D"
+                            label="D"
+                        />
+                        <button type="submit">Submit</button>
 
-                        </Form>
-                    </div>
-                </Formik>
+                    </Form>
+                </div>
+            </Formik>
 
-            );
-
-        }
-
+        );
 
     }
+
+
+}
 
 export default FormikClass
